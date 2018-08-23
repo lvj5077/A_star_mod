3d9e6810a06b7c232144c54b49a97ece0029b20c
Lingqiu Jin <lingqiujin@gmail.com>
2018-08-23 13:58:30 -0400

    modified for indoor map

---
+++ README.md
@@ -0,0 +1,40 @@
+I changed the resolution, diagonal cost
+use erosion to narrow the hallway for moving the path to the center
+
+
+# A-star
+A* pathfinding algorithm (a.k.a. A* search algorithm) written in C++
+
+This is an widely used pathfinding and searching algorithm; learn more about A* at [Wikipedia](https://en.wikipedia.org/wiki/A*_search_algorithm).
+
+OpenCV is not necessarily required, provided that you have found another way to convert input images into arrays which have the same format used in the example.
+
+Since there might still be some problems in my implementation, so please feel free to give me any advice!
+
+
+### License
+Beloved **MIT**
+
+```
+MIT License
+
+Copyright (c) 2017 Makito Sumi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```# A_star_pic
---
+++ build/CMakeFiles/Astar.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jin/Lingqiu_Jin/A_star_pic/main.cpp" "/home/jin/Lingqiu_Jin/A_star_pic/build/CMakeFiles/Astar.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/build"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/include/opencv"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/core/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/flann/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/imgproc/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/ml/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/objdetect/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/photo/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/video/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/viz/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/dnn/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/imgcodecs/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/shape/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/videoio/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/highgui/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/superres/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/ts/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/features2d/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/calib3d/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/stitching/include"
+  "/home/jin/Third_Party_Packages/opencv-3.4.2/modules/videostab/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
